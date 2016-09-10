7668599f161e75d03bc069c128db69726e1ed27b
Matteo Sticco <matteo.sticco@gmail.com>
2016-09-10 23:17:12 +0200

    Added slider to change volume.

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.slider import Slider
 
 # module to handle audio files
 from kivy.core.audio import SoundLoader
@@ -15,7 +16,7 @@ class MusicPlayer(GridLayout):
     def __init__(self, **kwargs):
        super(MusicPlayer, self).__init__(**kwargs)
        self.cols = 2
-       self.row = 1
+       self.row = 2
        self.play_pause = Button(text='Play/Pause')
        self.play_pause.bind(on_press=self.play_pause_song)
        self.add_widget(self.play_pause)
@@ -23,6 +24,10 @@ class MusicPlayer(GridLayout):
        self.stop = Button(text='Stop')
        self.stop.bind(on_press=self.stop_song)
        self.add_widget(self.stop)
+
+       self.volume_slider = Slider(min=0, max=1, step=0.01, value=0.5)
+       self.volume_slider.bind(value=self.set_volume)
+       self.add_widget(self.volume_slider)
 
        self.song = SoundLoader.load("/home/matteo/Music/Sigala_-_Sweet_Lovin_(Official_Audio)_mp3.mp3")
    
@@ -54,6 +59,10 @@ class MusicPlayer(GridLayout):
     def stop_song(self, instance):
        self.elapsed = 0
        self.song.stop()
+
+    def set_volume(self, instance, value):
+       self.song.volume = value
+       #print self.value
        
 
 class TestApp(App):
