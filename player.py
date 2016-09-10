475404ba319b29cf53f427921d70bf5d6a4d6712
Matteo Sticco <matteo.sticco@gmail.com>
2016-09-10 20:03:43 +0200

    Implemented play/pause and stop logic. Cleaned code a little bit.

--- player.py
+++ player.py
@@ -10,38 +10,55 @@ class MusicPlayer(GridLayout):
 
     play = False
     song = None
+    elapsed = 0
 
     def __init__(self, **kwargs):
        super(MusicPlayer, self).__init__(**kwargs)
-       self.cols = 1
+       self.cols = 2
        self.row = 1
-       self.play_stop = Button(text='Play/Stop')
-       self.play_stop.bind(on_press=self.play_stop_song)
-       self.add_widget(self.play_stop)
+       self.play_pause = Button(text='Play/Pause')
+       self.play_pause.bind(on_press=self.play_pause_song)
+       self.add_widget(self.play_pause)
+
+       self.stop = Button(text='Stop')
+       self.stop.bind(on_press=self.stop_song)
+       self.add_widget(self.stop)
 
        self.song = SoundLoader.load("/home/matteo/Music/Sigala_-_Sweet_Lovin_(Official_Audio)_mp3.mp3")
+   
+       # It seems to be a reasonable value for now
+       self.song.volume = 0.3
+
        
+    ''' Playing or pausing a song
+        1) If stopped play it again from the 'elapsed' value.
+           If it's 0 play it from the beginning.
+        2) If it's playing store the elapsed time and stop the song.
+    '''
+    def play_pause_song(self, instance):
 
-    def play_stop_song(self, instance):
-       print "button toggled"
-       self.play = not self.play
+       if self.song.state == 'stop':
+          print "Play or resume"
+          self.song.play()
+          self.song.seek(self.elapsed)
+       elif self.song.state == 'play':
+          print "Stopping.."
+          self.elapsed = self.song.get_pos()
+          self.song.stop()
 
-       self.song.play() if self.play else self.song.stop()
+    ''' Stopping the song
+        1) self.elapsed set to 0 so the next song (or the same)
+           will be played from the beginning
+        2) actually stop the song
+    '''    
+    def stop_song(self, instance):
+       self.elapsed = 0
+       self.song.stop()
        
-
 
 class TestApp(App):
     def build(self):
         return MusicPlayer()
-        '''sound = SoundLoader.load("/home/matteo/Music/Sigala_-_Sweet_Lovin_(Official_Audio)_mp3.mp3")
-
-        if sound:
-           print "Sound found at %s" % (sound.source)
-           print "Sound is %.3f seconds" % (sound.length)
-           print str(type(sound))
-           #sound.play()
-
-        return Button(text='Hello World')'''
 
 if __name__ == '__main__':
     TestApp().run()
