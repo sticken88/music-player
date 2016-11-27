aaa36dc2d6742a5f5ea52f5dafb26f0e772096d4
Matteo Sticco <matteo.sticco@gmail.com>
2016-11-27 13:03:58 +0100

    Added json parsing file to get a dictionary of all the available radio stations.

--- radio/radio.py
+++ radio/radio.py
@@ -1,6 +1,7 @@
 import pygst
 pygst.require('0.10')
 import gst
+import json
 
 class RadioPlayer(object):
 	"""docstring for RadioPlayer"""
@@ -8,19 +9,30 @@ class RadioPlayer(object):
 		self.pipeline = gst.Pipeline("RadioPipe")
 		self.player = gst.element_factory_make("playbin2", "player")
 
+        # json configuration file which holds the radio stations url
+		self.radio_stations_file = 'radio_stations.json'
+
 		if (not self.pipeline or not self.player):
 			print 'Not all elements could be created. Cannot create a Gstreamer pipeline to stream radio...'
 			exit(-1)
 
 		self.pipeline.add(self.player)
+		print "Created GStreamer pipleine..."
 
 		bus = self.pipeline.get_bus()
 		bus.add_signal_watch()
 		bus.connect("message", self.on_message)
 
+		#load radio stations
+		with open(self.radio_stations_file) as file_stations:
+			self.stations = json.load(file_stations)
 
-	def play_station(self, radioStation):
-		self.player.set_property('uri', radioStation)
+		print "Loaded radio stations file..."
+		self.radio_stations = self.stations['radio_stations']
+
+
+	def play_station(self, radio):
+		self.player.set_property('uri', self.radio_stations[radio])
 		self.pipeline.set_state(gst.STATE_PLAYING)
 		# getting the bus
 
