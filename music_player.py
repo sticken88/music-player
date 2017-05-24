7a34161ffc7f326d630f98f34d3f7ec9b99684ec
Matteo Sticco <matteo.sticco@gmail.com>
2017-05-24 21:56:57 +0200

    Deeply changed MusicPlayer class in order to use playbin. This allows mutiple audio encoding handling.

--- music_player.py
+++ music_player.py
@@ -1,70 +1,50 @@
-import pygst
-pygst.require('0.10')
 import gst
 import time
 
 class MusicPlayer():
 
    def __init__(self):
+      # initialize playbin element to reproduce different audio encoding
+      self.player = gst.element_factory_make("playbin", "player")
+      # set the initial default value to 0.5
+      self.player.set_property('volume', 0.5)
 
       # flag used to switch between play and pause
       self.playing = False
       # the current (or last) song being played
       self.current_song = None
-      # Create a Gstreamer pipeline
-      self.pipeline = gst.Pipeline('pipeline')
 
-      # Create the elements that will be added to the pipeline
-      self.audio_source = gst.element_factory_make('filesrc', 'audio_source')
-      self.decode = gst.element_factory_make('mad', 'decode')
-      self.convert = gst.element_factory_make('audioconvert', 'convert')
-      self.equalizer = gst.element_factory_make('equalizer-3bands', 'equalizer')
-      self.audio_sink = gst.element_factory_make('autoaudiosink', 'audio_sink')
-      self.volume = gst.element_factory_make('volume')
-      self.volume.set_property('volume', 0.5)
+      if (not self.player):
+         print 'Not all elements could be created. Cannot create a Gstreamer pipeline to stream songs...'
+         exit(-1)
 
-      # Ensure all elements were created successfully.
-      if (not self.pipeline or not self.audio_source or not self.decode or 
-          not self.convert or not self.equalizer or not self.audio_sink or
-          not self.volume):
-            print 'Not all elements could be created. Cannot create a Gstreamer pipeline'
-            exit(-1)
+      #self.pipeline.add(self.player)
+      print "Created GStreamer playbin..."
+      print "...default volume set to 0.5"
 
-      # Setting equalizer prpoperties
-      #self.equalizer.set_property('band1', -24.0)
-      #self.equalizer.set_property('band2', -24.0)
-
-      self.pipeline.add(self.audio_source, self.decode, self.convert, self.equalizer, self.audio_sink, self.volume)
-
-      # Add our elements to the pipeline
-      if (not gst.element_link_many(self.audio_source, self.decode, self.convert, self.volume,
-          self.equalizer, self.audio_sink)):
-            print "Elements could not be linked."
-            exit(-1)
-
-      # manage the bus (not sure if it's the right place)
-      bus = self.pipeline.get_bus()
+      bus = self.player.get_bus()
+      #bus.enable_sync_message_emission()
       bus.add_signal_watch()
+      # message::tag should give us only the tags
       bus.connect("message", self.on_message)
-
-      '''msg = bus.timed_pop_filtered(gst.CLOCK_TIME_NONE,
-            gst.MESSAGE_ERROR | gst.MESSAGE_EOS)
-      print msg'''
-      #gst_bus_add_watch (bus, my_bus_callback, NULL);
 
 
    def load_audio(self, audioResource):
+      # audioResource is the full path of the song in the file system
       self.current_song = audioResource
+      #print self.current_song
       self.stop_audio()
-      self.audio_source.set_property('location', audioResource)
+      #self.audio_source.set_property('location', audioResource)
+      self.player.set_property('uri', "file://"+self.current_song)
       self.play_audio()
 
 
    def play_audio(self):
       # Play the song
-      self.pipeline.set_state(gst.STATE_PLAYING)
+      #self.pipeline.set_state(gst.STATE_PLAYING)
+      self.player.set_state(gst.STATE_PLAYING)
       self.playing = True
-      print "Play the stream.."
+      print "Playing audio.."
 
 
       '''self.bus = self.pipeline.get_bus()
@@ -74,15 +54,16 @@ class MusicPlayer():
 
    
    def pause_audio(self):
-      self.pipeline.set_state(gst.STATE_PAUSED)
+      #self.pipeline.set_state(gst.STATE_PAUSED)
+      self.player.set_state(gst.STATE_PAUSED)
       self.playing = False
-      print "Pausing the stream.."
+      print "Pausing audio.."
 
 
    def stop_audio(self):
-      self.pipeline.set_state(gst.STATE_READY)
+      self.player.set_state(gst.STATE_READY)
       self.playing = False
-      print "Stopping the stream.."
+      print "Stopping audio.."
 
 
    def play_pause_audio(self):
@@ -96,7 +77,8 @@ class MusicPlayer():
            self.load_audio(self.current_song)
 
    def set_volume(self, volume):
-       self.volume.set_property('volume', volume)
+       #self.volume.set_property('volume', volume)
+       self.player.set_property('volume', volume)
 
 
    def on_message(self, bus, message):
